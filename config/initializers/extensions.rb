fabc7bf577e3326e1ce6fb3a847affff11dd1f9f
Jens Kraemer <jk@jkraemer.net>
2013-03-23 11:28:47 +0100

    fix regexp

--- config/initializers/extensions.rb
+++ config/initializers/extensions.rb
@@ -1,5 +1,5 @@
 class String
   def sluganize
-    self.downcase.gsub(/[\s.\/_]/, ' ').squeeze(' ').strip.gsub(/[^\w- ]/, '').tr(' ', '-')
+    self.downcase.gsub(/[\s.\/_]/, ' ').squeeze(' ').strip.gsub(/[^\w\s-]/, '').tr(' ', '-')
   end
 end
